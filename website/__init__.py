0b77a038a89e30d962adbedba90a20605f7341b3
abhinash6676 <abhinashsatapathy28@gmail.com>
2024-01-17 20:59:43 +0530

    trial

--- website/__init__.py
+++ website/__init__.py
@@ -2,4 +2,12 @@ from flask import Flask
 def create_app():
     app = Flask(__name__) 
     app.config['SECRET KEY']='i need a job.'
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+
+
     return app
