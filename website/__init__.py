292f8aaa6af85c589f68a9e67adaa09b0ff5c036
abhinash6676 <abhinashsatapathy28@gmail.com>
2024-01-22 20:43:23 +0530

    outdated sql db code replaced

--- website/__init__.py
+++ website/__init__.py
@@ -1,20 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 def create_app():
-    app = Flask(__name__)  
-    app.config['SECRET_KEY']='key123task'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
     db.init_app(app)
+
+    from . import models
+
+    with app.app_context():
+        db.create_all()
     #registering the blueprints:
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-
 
 
     return app
--- website/models.py
+++ website/models.py
@@ -6,7 +6,7 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #foreign key must use all letters in lower case, therefore 'user'.
 
 
 class User(db.Model, UserMixin):
@@ -14,4 +14,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150),unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    notes = db.relationship('Note') #relationship will use uppercase 'N' in Note, unlike foreign key.
